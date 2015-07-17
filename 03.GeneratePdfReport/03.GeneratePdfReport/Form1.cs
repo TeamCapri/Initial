1593456f903c1ae1fa306182e07490678e33d459
A-Mitev <amitev03@gmail.com>
2015-07-17 12:38:06 +0300

    Test Export

---
+++ 03.GeneratePdfReport/03.GeneratePdfReport/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+
+
+namespace _03.GeneratePdfReport
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Document doc = new Document(iTextSharp.text.PageSize.LETTER, 10, 10, 42, 35);
+            PdfWriter wri = PdfWriter.GetInstance(doc, new FileStream("Sales Report.pdf", FileMode.Create));
+            doc.Open();
+         
+            Paragraph paragraph = new Paragraph("Test Results.");
+            doc.Add(paragraph);
+            doc.Close();
+            
+
+        }
+
+    }
+}
