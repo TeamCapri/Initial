ce52cf8cb67085b5c248ce0f882fec0803bdeb24
A-Mitev <amitev03@gmail.com>
2015-07-17 18:09:41 +0300

    update

--- 03.GeneratePdfReport/03.GeneratePdfReport/Form1.cs
+++ 03.GeneratePdfReport/03.GeneratePdfReport/Form1.cs
@@ -20,20 +20,32 @@ namespace _03.GeneratePdfReport
         {
             InitializeComponent();
         }
-        
-
         private void button1_Click(object sender, EventArgs e)
         {
             Document doc = new Document(iTextSharp.text.PageSize.LETTER, 10, 10, 42, 35);
             PdfWriter wri = PdfWriter.GetInstance(doc, new FileStream("Sales_Report.pdf", FileMode.Create));
             doc.Open();
-         
-            Paragraph paragraph = new Paragraph("Test Results.");
-            doc.Add(paragraph);
+            PdfPTable table = new PdfPTable(5);
+            PdfPCell cell = new PdfPCell(new Phrase("Aggregated Sales Report"));
+            cell.Colspan = 5;
+            cell.HorizontalAlignment = 1; //0=Left, 1=Centre, 2=Right
+            table.AddCell(cell);
+            PdfPCell Date = new PdfPCell(new Phrase("Date"));
+            Date.Colspan = 5;
+            Date.HorizontalAlignment = 1; //0=Left, 1=Centre, 2=Right
+            table.AddCell(Date);
+            table.AddCell("Product");
+            table.AddCell("Quantity");
+            table.AddCell("Unit Price");
+            table.AddCell("Location");
+            table.AddCell("Sum");
+            PdfPCell TotalSum = new PdfPCell(new Phrase("TotalSum"));
+            TotalSum.Colspan = 4;
+            TotalSum.HorizontalAlignment = 1; //0=Left, 1=Centre, 2=Right
+            table.AddCell(TotalSum);
+            table.AddCell("Sum");
+            doc.Add(table);
             doc.Close();
-            
-
         }
-
     }
 }
