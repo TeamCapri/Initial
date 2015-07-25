e341ebd04045706e7434d2022ba17f98fcde4942
Dimitar <dimitar@dimitrov-bg.info>
2015-07-25 22:15:05 +0300

    Task 04

---
+++ DB_TeamCapri/FormMainEntry.cs
@@ -0,0 +1,19 @@
+﻿namespace DB_TeamCapri
+{
+    using System;
+    using System.Windows.Forms;
+
+    static class FormMainEntry
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+    }
+}
